015f2f8479e5a70e4b4ea6eb1d476344e0f98931
Tristan <tantris.kantner@gmail.com>
2021-05-31 09:40:14 +0200

    Clean default

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+const headerStyles = css`
+  display: flex;
+  padding: 10px 15px;
+  background-color: #ddd;
+  margin-bottom: 20px;
+  a + a {
+    margin-left: 15px;
+  }
+`;
+
+export default function Header() {
+  return (
+    <header css={headerStyles}>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/products">
+        <a>Products</a>
+      </Link>
+
+      {/* <h3>{props.guestList}</h3> */}
+    </header>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { css, Global } from '@emotion/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+          body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+          }
+        `}
+      />
+
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
